2e019cc3e8125d2ec9443033eba599288e233cda
Fabien Richard <fabien.richard@etu.univ-nantes.fr>
2013-05-22 09:29:29 +0200

    Suppression de tout

--- src/moduleConnexion/ModeleConnexion.java
+++
@@ -1,9 +0,0 @@
-package moduleConnexion;
-
-import accesBDD.ConnexionBDD;
-
-public class ModeleConnexion {
-	public static String essayerConnexion(String email, String mdp){
-		return ConnexionBDD.essayerConnexion(email, mdp);
-	}
-}
